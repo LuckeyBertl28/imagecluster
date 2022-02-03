5a09fde870dd5230f4343b9f55d0e9807611f414
Lukáš Bertl <lukas.bertl@cvut.cz>
2022-02-03 12:51:21 +0100

    Added argument to visualize N images from each cluster.

--- imagecluster/postproc.py
+++ imagecluster/postproc.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from typing import Dict, Sequence, Optional, Any
 
 from matplotlib import pyplot as plt
 import numpy as np
@@ -9,7 +10,13 @@ from . import calc as ic
 pj = os.path.join
 
 
-def plot_clusters(clusters, images, max_csize=None, mem_limit=1024**3):
+def plot_clusters(
+        clusters,
+        images,
+        max_csize: Optional[int] = None,
+        mem_limit: int = 1024**3,
+        n_examples: Optional[int] = None
+):
     """Plot `clusters` of images in `images`.
 
     For interactive work, use :func:`visualize` instead.
@@ -24,37 +31,51 @@ def plot_clusters(clusters, images, max_csize=None, mem_limit=1024**3):
         hard memory limit for the plot array (default: 1 GiB), increase if you
         have (i) enough memory, (ii) many clusters and/or (iii) large
         max(csize) and (iv) max_csize is large or None
+    n_examples : int, optional
+        show max n image examples from each cluster
     """
     assert len(clusters) > 0, "`clusters` is empty"
+
     stats = ic.cluster_stats(clusters)
     if max_csize is not None:
-        stats = stats[stats[:,0] <= max_csize, :]
+        stats = stats[stats[:, 0] <= max_csize, :]
+
     # number of clusters
-    ncols = stats[:,1].sum()
-    # csize (number of images per cluster)
-    nrows = stats[:,0].max()
+    n_columns = stats[:, 1].sum()
+
+    # cluster_size (number of images per cluster)
+    max_rows = stats[:, 0].max()
+    n_rows = max_rows if (n_examples is None) else min(n_examples, max_rows)
     shape = images[list(images.keys())[0]].shape[:2]
-    mem = nrows * shape[0] * ncols * shape[1] * 3
+
+    mem = n_rows * shape[0] * n_columns * shape[1] * 3
     if mem > mem_limit:
-        raise Exception(f"size of plot array ({mem/1024**2} MiB) > mem_limit "
-                        f"({mem_limit/1024**2} MiB)")
+        raise Exception(
+            f"size of plot array ({mem/1024**2} MiB) > mem_limit ({mem_limit/1024**2} MiB)"
+        )
+
     # uint8 has range 0..255, perfect for images represented as integers, makes
     # rather big arrays possible
-    arr = np.ones((nrows*shape[0], ncols*shape[1], 3), dtype=np.uint8) * 255
-    icol = -1
-    for csize in stats[:,0]:
-        for cluster in clusters[csize]:
-            icol += 1
-            for irow, filename in enumerate(cluster):
+    clusters_image = np.ones((n_rows*shape[0], n_columns*shape[1], 3), dtype=np.uint8) * 255
+    column_idx = -1
+    for cluster_size in stats[:, 0]:
+        for cluster in clusters[cluster_size]:
+            column_idx += 1
+            for row_idx, filename in enumerate(cluster):
                 image = images[filename]
-                arr[irow*shape[0]:(irow+1)*shape[0],
-                    icol*shape[1]:(icol+1)*shape[1], :] = image
-    print(f"plot array ({arr.dtype}) size: {arr.nbytes/1024**2} MiB")
-    fig,ax = plt.subplots()
-    ax.imshow(arr)
+                row_min = row_idx * shape[0]
+                row_max = (row_idx + 1) * shape[0]
+                column_min = column_idx * shape[1]
+                column_max = (column_idx + 1) * shape[1]
+                clusters_image[row_min:row_max, column_min:column_max, :] = image
+
+    print(f"plot array ({clusters_image.dtype}) size: {clusters_image.nbytes/1024**2} MiB")
+    fig, ax = plt.subplots()
+    ax.imshow(clusters_image)
     ax.axis('off')
     fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
-    return fig,ax
+
+    return fig, ax
 
 
 def visualize(*args, **kwds):
